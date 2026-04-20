f810860f9860dfb2289f6c1e4e1351c372dc37e0
ramiro <raspunsell0905@gmail.com>
2026-04-19 18:50:15 -0600

    Tercer commit. Tarjeta estilos separados

--- src/App.jsx
+++ src/App.jsx
@@ -8,50 +8,26 @@ function App() {
   const mensaje = edad >= 18 ? "Eres mayor de edad" : "Eres menor de edad";
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#1e3a8a",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "#3b82f6",
-          padding: "25px",
-          borderRadius: "15px",
-          width: "300px",
-          textAlign: "center",
-          boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
-          border: "2px solid #1d4ed8",
-        }}
-      >
-        <h1 style={{ color: "white" }}>Mi perfil</h1>
+    <div className =  "contenedor">
+      <div className= "tarjeta">
+        <h1 >Mi perfil</h1>
 
         <img
-          src="/public/imagenes/yo.png"
+          src="/imagenes/yo.png"
           alt="Mi foto"
-          style={{
-            width: "150px",
-            height: "150px",
-            borderRadius: "50%",
-            objectFit: "cover",
-            marginBottom: "10px",
-          }}
+          className = "imagen"
         />
 
-        <p style={{ color: "white" }}>
+        <p>
           <strong>Nombre:</strong> {nombre}
         </p>
-        <p style={{ color: "white" }}>
+        <p >
           <strong>Carrera:</strong> {carrera}
         </p>
-        <p style={{ color: "white" }}>
+        <p >
           <strong>Edad:</strong> {edad}
         </p>
-        <p style={{ color: "white" }}>{mensaje}</p>
+        <p >{mensaje}</p>
       </div>
     </div>
   );
